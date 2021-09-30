164461857b0e14c094db255e5b2557c811c24125
Štefan Jambrich <stefan.jambrich@student.spsmb.cz>
2021-09-30 12:49:44 +0200

    added not working answer chaning

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 //Function for fetching data from a json, returns object
 function fetchQnA(i) {
-  let qna = {
-    "question": "",
-    "correctA": "",
-    "incorrectA1": "",
-    "incorrectA2": "",
-    "incorrectA3": ""
-  };
+  let qna = [];
 
   fetch("qna.json")
-  .then(function (res) {
-    return res.json();
-  })
+  .then(function (res) {return res.json();})
   .then(function (data) {
+    let j = 0;
     for (let key in data["qna"][`qna${i}`]) {
-      qna[key] = data["qna"][`qna${i}`][key];
+      qna[j] = data["qna"][`qna${i}`][key];
+      j++;
     }
   })
 
   return qna;
 }
 
+const randomNum = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+let answers = document.querySelectorAll('.answers > *');
+let question = document.querySelector('.question');
+let prizes = document.querySelectorAll('.moneyCheck > *');
+let fiftyFifty = document.querySelector('.fiftyFifty');
 
-let qnaMainOBJ = fetchQnA(1)
+console.log(fetchQnA(1))
+
+window.onload = () => {
+  qna = fetchQnA(1);
+  for (let i = 0; i < answers.length; i++) {
+    console.log('negr')
+    answers[i].innerText = qna[i];
+  }
+}
