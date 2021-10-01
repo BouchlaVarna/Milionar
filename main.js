c021a6601d20f25f86fddae5da314b0dbe3a46b6
Štefan Jambrich <90748499+BouchlaVarna@users.noreply.github.com>
2021-10-02 00:15:15 +0200

    New features
    Added functions for reseting game and checking if set of questions and answers has already been generated, if so generate new one. Fixed color chaning of prizes depending on answer.
    
    TODO:
    Check if user has won.
    50/50 - create from sratch and make it so its only once per game

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ let question = document.querySelector('.question');
 let prizes = document.querySelectorAll('.moneyCheck > *');
 let fiftyFifty = document.querySelector('.fiftyFifty');
 let questsAndAns = [];
-let j = 0;
+let currentPrize = prizes.length;
+let hiddenVariable = "";
+let answersUsed = [0];
 
 const jsonToArray = (i) => {
   j = 0;
@@ -28,22 +30,54 @@ const randomNum = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+const checkIfUsed = (min, max) => {
+  let num = randomNum(min, max);
+  for (let i = 0; i < answersUsed.length; i++) {
+    if(num == answersUsed[i]){
+      num = randomNum(min, max);
+      i = 0;
+    }
+  }
+  console.log(answersUsed)
+  answersUsed.push(num)
+  return num;
+}
+
 let qnaGen = () => {
   for (let i = 0; i < answers.length; i++) {
     answers[i].innerText = questsAndAns[i];
   }
+  hiddenVariable = questsAndAns[0]
   question.innerText = questsAndAns[4];
 }
 
 let checkAnswer = (answer, correctAnswer) => {
-  jsonToArray(randomNum(1, 3))
+  jsonToArray(checkIfUsed(1, 3));
   qnaGen();
+  if(answer == correctAnswer){
+    currentPrize--;
+    prizes[currentPrize].style.backgroundColor = 'blue';
+    return;
+  }
+  resetGame();
+}
+
+let resetGame = () => {
+  answersUsed = [];
+  currentPrize = prizes.length;
+  prizes.forEach(element => {
+    element.style.backgroundColor = ''
+  });
+}
+
+let winningCondition = () => {
+  
 }
 
 answers.forEach(element => {
   element.onclick = () => {
-    checkAnswer(element.innerText, questsAndAns[0])
+    checkAnswer(element.innerText, hiddenVariable)
   }
 });
 
-window.onload = qnaGen, jsonToArray(randomNum(1, 3)); 
+window.onload = qnaGen, jsonToArray(checkIfUsed(1, 3)); 
