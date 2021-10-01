e2ae481fe6f9f15c279554a538cdfefa8b73c1af
Štefan Jambrich <90748499+BouchlaVarna@users.noreply.github.com>
2021-10-01 21:36:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,49 @@
-//Function for fetching data from a json, returns object
-function fetchQnA(i) {
-  let qna = [];
+let answers = document.querySelectorAll('.answers > *');
+let question = document.querySelector('.question');
+let prizes = document.querySelectorAll('.moneyCheck > *');
+let fiftyFifty = document.querySelector('.fiftyFifty');
+let questsAndAns = [];
+let j = 0;
 
-  fetch("qna.json")
-  .then(function (res) {return res.json();})
-  .then(function (data) {
-    let j = 0;
-    for (let key in data["qna"][`qna${i}`]) {
-      qna[j] = data["qna"][`qna${i}`][key];
+const jsonToArray = (i) => {
+  j = 0;
+  let fetchQnA = async (i) => {
+    fetchFile = await fetch('qna.json');
+    let data = await fetchFile.json();
+  
+    createQnA(data, i);
+  }
+  
+  fetchQnA(i);
+  
+  let createQnA = (data, i) => {
+    for (const key in data["qna"][`qna${i}`]) {
+      questsAndAns[j] = (data["qna"][`qna${i}`][key]);
       j++;
     }
-  })
-
-  return qna;
+  }
 }
 
 const randomNum = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let answers = document.querySelectorAll('.answers > *');
-let question = document.querySelector('.question');
-let prizes = document.querySelectorAll('.moneyCheck > *');
-let fiftyFifty = document.querySelector('.fiftyFifty');
-
-console.log(fetchQnA(1))
-
-window.onload = () => {
-  qna = fetchQnA(1);
+let qnaGen = () => {
   for (let i = 0; i < answers.length; i++) {
-    console.log('negr')
-    answers[i].innerText = qna[i];
+    answers[i].innerText = questsAndAns[i];
   }
+  question.innerText = questsAndAns[4];
 }
+
+let checkAnswer = (answer, correctAnswer) => {
+  jsonToArray(randomNum(1, 3))
+  qnaGen();
+}
+
+answers.forEach(element => {
+  element.onclick = () => {
+    checkAnswer(element.innerText, questsAndAns[0])
+  }
+});
+
+window.onload = qnaGen, jsonToArray(randomNum(1, 3)); 
